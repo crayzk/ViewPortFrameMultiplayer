a6332c70ab8204040bdcd36b500fc9d8f9c963f2
Josh <me@joshl.io>
2018-12-29 17:55:05 +0000

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,36 @@
-local RBX_MULTIPLAYER = {} -- Default code stuffz
+local Multiplayer = {}
+local MaxPlayers = 4
+local PlayerCount = 1
 
-return RBX_MULTIPLAYER
+function Multiplayer:AddPlayer(Gui, Controller)
+	-- Gui: ScreenGui to contain splitscreen
+	-- Controller: Controller to be used
+	if PlayerCount < MaxPlayers then
+		Gui:ClearAllChildren()
+		PlayerCount = PlayerCount + 1
+		local SplitScreen = script[tostring(PlayerCount)]:Clone()
+		
+		SplitScreen.Parent = Gui
+	else
+		warn("Too many players, request ignored! Max is " .. MaxPlayers)
+	end
+end
+
+function Multiplayer:RemovePlayer(Gui)
+	-- Gui: ScreenGui to contain splitscreen
+	if PlayerCount > 1 then
+		Gui:ClearAllChildren()
+		PlayerCount = PlayerCount - 1
+		if PlayerCount >= 2 then
+			local SplitScreen = script[tostring(PlayerCount)]:Clone()
+			
+			SplitScreen.Parent = Gui
+		end
+	else
+		warn("Not enough players to remove, request ignored!")
+	end
+end
+
+return Multiplayer
+
+-- HELLO JOSHRBX WAS HERE
