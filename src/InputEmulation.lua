30adb50e7a1a7b9b955dd8096d28e1a474cc84f1
crayzk <46233427+crayzk@users.noreply.github.com>
2019-01-03 15:35:17 -0600

    Update InputEmulation.lua

--- src/InputEmulation.lua
+++ src/InputEmulation.lua
@@ -1,11 +1,10 @@
---[[ PARENTED to MAIN.lua ]]--
+--[[ CHILD of MAIN.lua ]]--
 
 --[[
   The main purpose of this module is to emulate multiple cursor movements, using the joysticks, the mouse, among other binds
     -written by vivivio
     -briefing by crayzk (pls update vivivio)
 --]]
-
 
 local InputEmulator = {}
 -- [[ SERVICES ]] --
@@ -22,8 +21,8 @@ local CurEmulatorMode = EMULATOR_MODE.Keyboard -- Default emulator mode is keybo
 local BindedInputs = {} -- Inputs binded to functions
 
 -- [[ LOGIC ]] --
-function FlushTbl() -- Removes all previously binded functions
-	for i,_ in ipairs(BindedInputs) do
+function UnBindAll() -- Removes all previously binded functions
+	for i in ipairs(BindedInputs) do
 		table.remove(BindedInputs, i)
 	end
 end
@@ -31,60 +30,85 @@ end
 function InputEmulator:SetMode(EmulatorMode)
 	if EmulatorMode == "Gamepad" then
 		CurEmulatorMode = EMULATOR_MODE.Gamepad
-		FlushTbl() 
+		UnBindAll() 
 	elseif EmulatorMode == "Keyboard" then
 		CurEmulatorMode = EMULATOR_MODE.Keyboard
-		FlushTbl()
+		UnBindAll()
 	elseif EmulatorMode == "Tablet" then
 		CurEmulatorMode = EMULATOR_MODE.Tablet
-		FlushTbl()
+		UnBindAll()
 	end
 end
 
-function InputEmulator:Bind(BindName, KeyCode, CallBack, GamepadNum)
+function InputEmulator:Bind(BindName, InputType, CallBack, GamepadNum)
+	
+	if type(InputType) == "table" then
+		local InputObjs = {}
+		for _,v in ipairs(InputType) do -- Support for a list of inputs
+			table.insert(InputObjs, v)
+		end
+		InputType = InputObjs
+	end
+	
 	if CurEmulatorMode == EMULATOR_MODE.Keyboard then
-		table.insert(BindedInputs, {BindName, KeyCode, CallBack}) -- The name of the bind is [1], the keycode is [2] and the callback function is [3]
+		table.insert(BindedInputs, {BindName, InputType, CallBack}) -- The name of the bind is [1], the InputType { KeyCode or UserInputType } is [2] and the callback function is [3]
 	elseif CurEmulatorMode == EMULATOR_MODE.Gamepad then
-		table.insert(BindedInputs, {BindName, KeyCode, CallBack, GamepadNum}) -- The name of the bind is [1], the keycode is [2], the callback function is [3] and the Gamepad number is [4]
+		table.insert(BindedInputs, {BindName, InputType, CallBack, GamepadNum}) -- The name of the bind is [1], the keycode is [2], the callback function is [3] and the Gamepad number is [4]
 	elseif CurEmulatorMode == EMULATOR_MODE.Tablet then
-		-- NOT DONE REEEE
+		-- JOSHHHHHHH
 	end
 end
 
 function InputEmulator:UnBind(BindName)
-	for i,_ in ipairs(BindedInputs) do
-		if BindedInputs[i][3] == BindName then
+	for i in ipairs(BindedInputs) do
+		if BindedInputs[i][1] == BindName then
 			table.remove(BindedInputs, i)
 			break
 		end
 	end
 end
 
-function InputEmulator:CreateCursor() -- Will be used to emulate multiple joysticks and fingers
-	local CursorObj = {}
-	
-	function CursorObj:BindInputFrom()
-		
-	end
-	
-	return CursorObj
-end
+
+
+
 
 function InputEvt(InputObj)
+	
 	if CurEmulatorMode == EMULATOR_MODE.Keyboard then
 		for _,v in ipairs(BindedInputs) do
-			if v[2] == InputObj.KeyCode or v[2] == InputObj.UserInputType then -- or statement is there to register mouse events 
-				v[3](InputObj)
+			
+			if type(v[2]) ~= "table" then
+				if v[2] == InputObj.KeyCode or v[2] == InputObj.UserInputType then -- or statement is there to register mouse events 
+					if v[3] then
+						v[3](InputObj)
+					else
+						warn('WARNING!' .. v[2].Name .. ' function is missing or nil.')
+					end
+				end
+			else
+				for _,input in ipairs(v[2]) do -- Support for a list of inputs
+					if input == InputObj.KeyCode or input == InputObj.UserInputType then -- or statement is there to register mouse events 
+						v[3](InputObj)
+					end
+				end
 			end
 		end
 	elseif CurEmulatorMode == EMULATOR_MODE.Gamepad then
 		for _,v in ipairs(BindedInputs) do
-			if UserInputSvc:IsGamepadButtonDown(v[4], v[2]) then
-				v[3](InputObj)
+			if type(v[2]) ~= "table" then
+				if UserInputSvc:IsGamepadButtonDown(v[4], v[2]) then
+					v[3](InputObj)
+				end
+			else
+				for _,input in ipairs(v[2]) do -- Support for a list of inputs
+					if UserInputSvc:IsGamepadButtonDown(v[4], input) then
+						v[3](InputObj)
+					end
+				end
 			end
 		end
 	elseif CurEmulatorMode == EMULATOR_MODE.Tablet then
-		-- NOT DONE
+		-- JOSH GET OVER HERE
 	end
 end
 
